ec352d8ad9d598a6ec1d803d4026388f971a06dc
trmorningstar <trmorningstar@gmail.com>
2022-09-30 19:55:40 -0400

    add new migraine view

--- controllers/migraines.js
+++ controllers/migraines.js
@@ -2,8 +2,9 @@ import { Migraine } from '../models/migraine.js'
 
 function newMigraine(req, res) {
   console.log("test")
-  res.render('migraines/new')
+  res.render('migraines/new', {
   title: 'Add Migraine'
+})
 }
 
 export {
